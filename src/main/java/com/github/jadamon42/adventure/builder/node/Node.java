b989641ca814956ad27cf179a5031ea4ec792163
Jonathan Damon <john.damon@freightverify.com>
2024-08-25 00:57:32 -0400

    clean up node draggable logic

--- src/main/java/com/github/jadamon42/adventure/builder/node/Node.java
+++ src/main/java/com/github/jadamon42/adventure/builder/node/Node.java
@@ -7,30 +7,26 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.layout.VBox;
 
 class Node extends VBox {
+    private final DragContext dragContext;
+
     public Node() {
+        dragContext = new DragContext();
         makeDraggable();
     }
 
     private void makeDraggable() {
-        final Delta dragDelta = new Delta();
-
         setOnMousePressed(mouseEvent -> {
             if (mouseEvent.getButton() == MouseButton.PRIMARY) {
-                double scaleFactor = AppState.getInstance().getScaleFactor();
-                dragDelta.x = (mouseEvent.getSceneX() - (getLayoutX() * scaleFactor)) / scaleFactor;
-                dragDelta.y = (mouseEvent.getSceneY() - (getLayoutY() * scaleFactor)) / scaleFactor;
+                dragContext.startDrag(mouseEvent.getSceneX(), mouseEvent.getSceneY(), getLayoutX(), getLayoutY());
                 getScene().setCursor(Cursor.CLOSED_HAND);
             }
-
         });
 
         setOnMouseDragged(mouseEvent -> {
             mouseEvent.consume();
-            double scaleFactor = AppState.getInstance().getScaleFactor();
-            double newX = (mouseEvent.getSceneX() / scaleFactor) - dragDelta.x;
-            double newY = (mouseEvent.getSceneY() / scaleFactor) - dragDelta.y;
+            double newX = dragContext.calculateNewX(mouseEvent.getSceneX());
+            double newY = dragContext.calculateNewY(mouseEvent.getSceneY());
 
-            // Prevent the box from being dragged outside the main viewport bounds
             Bounds bounds = AppState.getInstance().getMainBoardBounds();
             double maxX = (bounds.getWidth() - getWidth());
             double maxY = (bounds.getHeight() - getHeight());
@@ -45,7 +41,22 @@ class Node extends VBox {
         setOnMouseReleased(mouseEvent -> getScene().setCursor(Cursor.DEFAULT));
     }
 
-    private static class Delta {
-        double x, y;
+    private static class DragContext {
+        private double offsetX, offsetY;
+        private double scaleFactor;
+
+        public void startDrag(double sceneX, double sceneY, double layoutX, double layoutY) {
+            scaleFactor = AppState.getInstance().getScaleFactor();
+            offsetX = (sceneX - (layoutX * scaleFactor)) / scaleFactor;
+            offsetY = (sceneY - (layoutY * scaleFactor)) / scaleFactor;
+        }
+
+        public double calculateNewX(double sceneX) {
+            return (sceneX / scaleFactor) - offsetX;
+        }
+
+        public double calculateNewY(double sceneY) {
+            return (sceneY / scaleFactor) - offsetY;
+        }
     }
 }
